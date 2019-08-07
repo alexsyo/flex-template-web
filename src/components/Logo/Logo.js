6505c8ea4bcf03e9bb5f0438a51512f520bdaa40
alexs <alex_ok@hotmail.it>
2019-08-08 01:35:03 +0200

    mobile logo updated

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 import config from '../../config';
-import IconLogo from './IconLogo';
 import LogoImage from './boxit-logo.svg';
-import css from './Logo.css';
+import LogoIconImage from './boxit-logo-icon.svg';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
-  const mobileClasses = classNames(css.logoMobile, className);
 
-  if (format === 'desktop') {
-    return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
-  }
+    return <img
+      className={className}
+      src={format === 'desktop' ? LogoImage : LogoIconImage}
+      alt={config.siteTitle}
+      {...rest}
+    />;
+  
 
-  return <IconLogo className={mobileClasses} {...rest} />;
 };
 
 const { oneOf, string } = PropTypes;
