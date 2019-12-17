37ea58fe2dd9ebce2b794242243ab246aa7c0b29
alexs <alex_ok@hotmail.it>
2019-12-17 23:22:56 +0100

    added italian dropdown menu

--- src/locale/config.js
+++ src/locale/config.js
@@ -1,10 +1,13 @@
 import enTrans from '../translations/en.json';
 import nlTrans from '../translations/nl.json';
+import itTrans from '../translations/it.json';
 
 import ukFlag from './svg/united-kingdom.svg';
 import nlFlag from './svg/netherlands.svg';
+import itFlag from './svg/italy.svg';
 
 export const translations = {
   en: { messages: enTrans, flag: ukFlag },
   nl: { messages: nlTrans, flag: nlFlag },
+  it: { messages: itTrans, flag: itFlag }
 }
