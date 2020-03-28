8c6c747d816d6f065dc48c72f40e0b7ce9698ed1
nikotrone <nikotrone@gmail.com>
2020-03-28 18:30:03 +0100

    move link to blog

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -104,6 +104,15 @@ const Footer = props => {
                   </NamedLink>
                 </li>
                 <li className={css.listItem}>
+                  <ExternalLink
+                    href="https://blog.boxitonline.com"
+                    className={css.link}
+                    title="Footer.goToBlog"
+                  >
+                    <FormattedMessage id="Footer.toBlogPage" />
+                  </ExternalLink>
+                </li>
+                <li className={css.listItem}>
                   <NamedLink name="AboutPage" className={css.link}>
                     <FormattedMessage id="Footer.toAboutPage" />
                   </NamedLink>
@@ -126,21 +135,6 @@ const Footer = props => {
               </ul>
             </div>
             <div className={css.searches}>
-              <p>
-                <ul className={css.list}>
-                  <li className={css.listItem}>
-                    <ExternalLink
-                      // key="linkToTwitter"
-                      href="https://blog.boxitonline.com"
-                      className={css.link}
-                      title="Footer.goToBlog"
-                    >
-                      <FormattedMessage id="Footer.toBlogPage" />
-                    </ExternalLink>
-                  </li>
-                </ul>
-              </p>
-
               <ul className={css.list}>
                 <li className={css.listItem}>
                   <NamedLink
